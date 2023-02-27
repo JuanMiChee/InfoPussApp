f48bd4bc7eb2b5a277fca9b80cc416e85bd86c0b
Juan Harrington <harringtonjuanc@gmail.com>
2023-02-26 19:43:28 -0500

    Initial Commit

---
+++ InfoPussApp/InfoPussAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfoPussAppApp.swift
+//  InfoPussApp
+//
+//  Created by Juan Harrington on 26/02/23.
+//
+
+import SwiftUI
+
+@main
+struct InfoPussAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
